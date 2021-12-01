1c570ec806c9c43f593eceef46d1139900d3db55
Esther Lozano <esloho@gmail.com>
2021-12-01 22:11:42 +0100

    Add small test

--- lib/day01/puzzle1.ex
+++ lib/day01/puzzle1.ex
@@ -9,6 +9,13 @@ defmodule Adventofcode2021.Day01.Puzzle1 do
     |> count_depth_increases()
   end
 
+  def convert_input(stream) do
+    stream
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.to_list()
+  end
+
   def count_depth_increases(measures) do
     measures
     |> Enum.reduce({0, -1}, &detect_increased/2)
@@ -17,13 +24,6 @@ defmodule Adventofcode2021.Day01.Puzzle1 do
 
   defp read_input(path) do
     File.stream!(path, [:utf8], :line)
-  end
-
-  defp convert_input(stream) do
-    stream
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.to_integer/1)
-    |> Enum.to_list()
   end
 
   defp detect_increased(measure, {count, -1}) do
--- test/day01/puzzle1_test.exs
+++ test/day01/puzzle1_test.exs
@@ -3,9 +3,18 @@ defmodule Adventofcode2021.Day01.Puzzle1Test do
 
   alias Adventofcode2021.Day01.Puzzle1
 
-  describe "analyze_depth_increases/1" do
+  @path "test/day01/input_test.txt"
 
-    @path "test/day01/input_test.txt"
+  describe "convert_input/1" do
+
+    test "returns measures" do
+      file_stream = File.stream!(@path, [:utf8], :line)
+
+      assert Puzzle1.convert_input(file_stream) == [131, 140, 136, 135, 155, 175, 178]
+    end
+  end
+
+  describe "analyze_depth_increases/1" do
 
     test "returns the number of increments" do
       assert Puzzle1.analyze_depth_increases(@path) == 4
